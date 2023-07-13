2b6442f6fd53e16c8d674fd57697d7e948363c32
tastefulblatant <121983707+tastefulblatant@users.noreply.github.com>
2023-07-13 21:17:27 +0200

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,10 @@
+@echo off
+echo MAKE SURE THAT YOU HAVE A PYTHON IN YOUR PATH!!
+
+
+pip install requests
+pip install tkinter 
+pip install customtkinter
+pip install Image
+
+echo Requirements was installed successfuly
